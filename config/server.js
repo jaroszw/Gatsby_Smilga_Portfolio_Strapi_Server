bc86bfb426704e0e841e1af246568abf3765e741
Wojciech Jarosz <jaroszw@gmail.com>
2021-04-27 18:52:55 +0200

    server for portfolio smigla project - initial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3ef19efb02be391468743dd11609b28f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '72ed4fbd-70d8-4f65-b55c-510b29b09047'
+};
